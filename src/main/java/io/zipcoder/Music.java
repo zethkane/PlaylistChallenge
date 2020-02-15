560ad63ca1a5a60a087405b5fd63cfebbc28c25b
Zeth Kane <zkane@zipcodes-MacBook-Pro-9.local>
2020-02-15 17:02:15 -0500

    finsihed

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -4,11 +4,37 @@ public class Music {
 
     private String[] playList;
 
-    public Music(String[] playList){
+    public Music(String[] playList) {
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+    public Integer selection(Integer startIndex, String selection) {
+        int back = 0;
+        int forward = 0;
+        int index = startIndex;
+        while (playList[index] != selection) {
+            if (index == playList.length) {
+                index = 0;
+                forward++;
+            } else {
+                index++;
+                forward++;
+            }
+        }
+        while (playList[startIndex] != selection) {
+            if (startIndex == 0) {
+                startIndex = playList.length - 1;
+                back++;
+            } else {
+                startIndex--;
+                back++;
+            }
+        }
+        if (forward < back) {
+            return forward;
+        } else {
+            return back;
+        }
+
     }
 }
